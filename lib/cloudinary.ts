4af0a337d110773452b26c76ffcaa5bdf5699efe
Hassool <mekkyot@gmail.com>
2026-01-24 16:25:37 +0100

    production ready

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -2,10 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // Configure Cloudinary
 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY ? 'Present' : 'Missing',
-    api_secret: process.env.CLOUDINARY_API_SECRET ? 'Present' : 'Missing',
-});
+
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
